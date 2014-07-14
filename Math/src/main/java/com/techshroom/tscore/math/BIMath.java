1fef1ec766596fc04386c6578cc4e8fedc7bec8a
Kenzie Togami <ket1999@gmail.com>
2014-07-14 07:17:03 -0700

    BigInteger helper classes

---
+++ Math/src/main/java/com/techshroom/tscore/math/BIMath.java
@@ -0,0 +1,29 @@
+package com.techshroom.tscore.math;
+
+import java.math.BigInteger;
+
+public final class BIMath {
+
+	private BIMath() {
+	}
+
+	public static boolean lessThan(BigInteger a, BigInteger b) {
+		return a.compareTo(b) < 0;
+	}
+
+	public static boolean lessThanEquals(BigInteger a, BigInteger b) {
+		return a.compareTo(b) <= 0;
+	}
+
+	public static boolean greaterThan(BigInteger a, BigInteger b) {
+		return a.compareTo(b) > 0;
+	}
+
+	public static boolean greaterThanEquals(BigInteger a, BigInteger b) {
+		return a.compareTo(b) >= 0;
+	}
+
+	public static boolean equals(BigInteger a, BigInteger b) {
+		return a.compareTo(b) == 0;
+	}
+}
---
+++ Math/src/main/java/com/techshroom/tscore/math/ExtraMath.java
@@ -0,0 +1,24 @@
+package com.techshroom.tscore.math;
+
+import static com.techshroom.tscore.math.BIMath.lessThan;
+
+import java.math.BigInteger;
+
+public class ExtraMath {
+
+	/**
+	 * Handles BigInteger factorial.
+	 * 
+	 * @param num1
+	 *            - factorial number
+	 * @return <tt>num1!</tt>
+	 */
+	public static BigInteger factorial(BigInteger num1) {
+		BigInteger one = BigInteger.ONE, fact = one;
+		for (BigInteger i = BigInteger.valueOf(2); lessThan(i, num1); i = i
+				.add(one)) {
+			fact = fact.multiply(i);
+		}
+		return fact;
+	}
+}
