639ce4d2d4f93f2023f2d51f2d398d62b099f558
Kenzie Togami <ket1999@gmail.com>
2014-07-14 19:09:07 -0700

    Some default methods

--- GeneralUtility/src/main/java/com/techshroom/tscore/util/GetterKey.java
+++ GeneralUtility/src/main/java/com/techshroom/tscore/util/GetterKey.java
@@ -15,4 +15,29 @@ public final class GetterKey<T> implements UncheckedCaster<T> {
 	public T cast(Object o) {
 		return (T) o;
 	}
+
+	@Override
+	public String toString() {
+		return new StringBuilder().append("Key<").append(name).append(">")
+				.toString();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+
+		if (obj instanceof GetterKey<?>) {
+			GetterKey<?> key = (GetterKey<?>) obj;
+			return key.name.equals(name);
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
 }
