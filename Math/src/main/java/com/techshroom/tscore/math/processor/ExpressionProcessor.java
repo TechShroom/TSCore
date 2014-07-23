f1d554e8218ff61b5bf554bf8e56d8ac079671fa
Kenzie Togami <ket1999@gmail.com>
2014-07-23 07:04:40 -0700

    About to rewrite parser

--- Math/src/main/java/com/techshroom/tscore/math/processor/ExpressionProcessor.java
+++ Math/src/main/java/com/techshroom/tscore/math/processor/ExpressionProcessor.java
@@ -11,7 +11,6 @@ import com.techshroom.tscore.math.exceptions.EvalException;
 import com.techshroom.tscore.math.exceptions.EvalException.Reason;
 import com.techshroom.tscore.math.operator.Operator;
 import com.techshroom.tscore.regex.MatchInfo;
-import com.techshroom.tscore.regex.MatchList;
 
 public abstract class ExpressionProcessor {
 	protected final String processing;
@@ -34,48 +33,14 @@ public abstract class ExpressionProcessor {
 		Matcher funcget = FUNCTION_GET_REGEX.matcher(processing);
 		Matcher opget = OPERATOR_GET_REGEX.matcher(processing);
 		// the functions match their numbers as well, be careful about that
-		// scan the numbers and mark positions
-		// match numbers to operators
-		List<MatchInfo> numMatches = new MatchList(numbers);
-		List<MatchInfo> funcMatches = new MatchList(funcget);
-		List<MatchInfo> opMatches = new MatchList(opget);
-		if (numMatches.size() == 0) {
-			// no numbers: invalid format
-			throw new EvalException(Reason.NO_NUMBERS);
-		}
-		if (opMatches.size() == 0) {
-			// no operators, required:
-			// one function or only one number
-			if (funcMatches.size() == 0) {
-				// no functions, this means there was just one or more numbers.
-				if (numMatches.size() == 1) {
-					tokens.add(numMatches.get(0).getMatch());
-					return;
-				} else {
-					// too many numbers
-					throw new EvalException(Reason.TOO_MANY_NUMBERS, 1,
-							numMatches.size());
-				}
+		if (numbers.lookingAt()) {
+			// first is number
+			// op next
+			if (!opget.lookingAt()) {
+				throw new EvalException(Reason.NEEDED_OPERATOR);
 			}
-		}
-
-		for (MatchInfo opMatch : opMatches) {
-			System.err.println(opMatch);
-		}
-
-		// by this point there are some functions or operators to work with
-		if (numMatches.get(0).getStart() == 0) {
-			// number first, which implies operator first
-			// matches are in order from string
-			MatchInfo firstOp = opMatches.get(0);
-			Operator check = Operator.getOperator(firstOp.getMatch());
-			// push a number
-			tokens.add(numMatches.get(0).getMatch());
-			tokens.add(check.toString());
-			if (numMatches.get(1).getStart() == firstOp.getEnd()) {
-				// not function
-				tokens.add(numMatches.get(1).getMatch());
-			}
+			
+			MatchInfo opMatchInfo = MatchInfo.saveState(opget);
 		}
 	}
 
