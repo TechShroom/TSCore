956209026482b13a226fd40d09091049bae1ee39
Kenzie Togami <ket1999@gmail.com>
2014-07-23 17:54:17 -0700

    Fiddling with regex

--- Math/src/main/java/com/techshroom/tscore/math/processor/RegexBits.java
+++ Math/src/main/java/com/techshroom/tscore/math/processor/RegexBits.java
@@ -5,7 +5,7 @@ public final class RegexBits {
 	 * Variable finder in the form @variableName, where variableName may contain
 	 * alpha-numeric values only and must not start with a number.
 	 */
-	public static final String VARIABLE = "(?:@[a-zA-Z][a-zA-Z0-9]*)";
+	public static final String VARIABLE = "(@[a-zA-Z][a-zA-Z0-9]*)";
 	/**
 	 * Function finder in the form functionName, where functionName may contain
 	 * alpha-numeric values only and must not start with a number.
@@ -18,11 +18,12 @@ public final class RegexBits {
 	/**
 	 * Number finder (float). Includes `E` matching and negative numbers.
 	 */
-	public static final String NUMBER_BASE = "([-+]?\\d+(\\.\\d+)?([eE][-+]?\\d+)?)";
+	public static final String NUMBER_BASE = "(([-+]?\\d+(?:\\.\\d+)?)"
+			+ "(?:[eE]([-+]?\\d+))?)";
 	/**
 	 * Operator regex to find just the operator
 	 */
-	public static final String OPERATOR = "[^a-zA-Z0-9]";
+	public static final String OPERATOR = "([^a-zA-Z0-9])";
 
 	private RegexBits() {
 	}
--- Math/src/main/java/com/techshroom/tscore/math/processor/RegexPatterns.java
+++ Math/src/main/java/com/techshroom/tscore/math/processor/RegexPatterns.java
@@ -4,18 +4,12 @@ import java.util.regex.Pattern;
 
 public final class RegexPatterns {
 	/**
-	 * Regex to find any numbers in a string.<br>
-	 * <br>
-	 * 
-	 * <code>REGEX=([-+]?\d+(\.\d+)?([eE][-+]?\d+)?)</code>
+	 * Regex to find any numbers in a string.
 	 */
-	protected static final Pattern NUMBER_REGEX = Pattern
+	public static final Pattern NUMBER_REGEX = Pattern
 			.compile(RegexBits.NUMBER_BASE);
 	/**
-	 * Regex to validate functions.<br>
-	 * <br>
-	 * 
-	 * <code>REGEX=(([a-zA-Z][a-zA-Z0-9]*)\(((?:@[a-zA-Z][a-zA-Z0-9]*)?(?:,\s*(?:@[a-zA-Z][a-zA-Z0-9]*))*)\))</code>
+	 * Regex to validate functions.
 	 */
 	public static final Pattern FUNCTION_VALIDATE_REGEX = Pattern.compile("("
 			+ RegexBits.FUNCTION + RegexBits.OPTSPACE + "\\(("
@@ -23,33 +17,18 @@ public final class RegexPatterns {
 			+ RegexBits.OPTSPACE + "," + RegexBits.OPTSPACE
 			+ RegexBits.VARIABLE + ")*)" + RegexBits.OPTSPACE + "\\))");
 	/**
-	 * Regex to find functions.<br>
-	 * <br>
-	 * 
-	 * <code>REGEX=(([a-zA-Z][a-zA-Z0-9]*)\(((?:([-+]?\\d+(\.\d+)?([eE][-+]?\d+)?))?(?:,\s*(?:([-+]?\\d+(\.\d+)?([eE][-+]?\\d+)?)))*)\))</code>
-	 */
+	 * Regex to find functions.
+	 * */
 	public static final Pattern FUNCTION_GET_REGEX = Pattern.compile("("
 			+ RegexBits.FUNCTION + RegexBits.OPTSPACE + "\\(("
 			+ RegexBits.OPTSPACE + RegexBits.NUMBER_BASE + "?(?:"
 			+ RegexBits.OPTSPACE + "," + RegexBits.OPTSPACE
 			+ RegexBits.NUMBER_BASE + ")*)" + RegexBits.OPTSPACE + "\\))");
 	/**
-	 * Regex to validate operators.<br>
-	 * <br>
-	 * 
-	 * <code>REGEX=(((?:@[a-zA-Z][a-zA-Z0-9]*))?[^a-zA-Z0-9]((?:@[a-zA-Z][a-zA-Z0-9]*))?)</code>
+	 * Regex to get operators.
 	 */
-	public static final Pattern OPERATOR_VALIDATE_REGEX = Pattern.compile("(("
-			+ RegexBits.VARIABLE + ")?" + RegexBits.OPERATOR + "("
-			+ RegexBits.VARIABLE + ")?)");
-	/**
-	 * Regex to get operators.<br>
-	 * <br>
-	 * 
-	 * <code>REGEX=([^a-zA-Z0-9])</code>
-	 */
-	public static final Pattern OPERATOR_GET_REGEX = Pattern.compile("("
-			+ RegexBits.OPERATOR + ")");
+	public static final Pattern OPERATOR_GET_REGEX = Pattern
+			.compile(RegexBits.OPERATOR);
 
 	private RegexPatterns() {
 	}
